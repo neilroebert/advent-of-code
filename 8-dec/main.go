199e56d2bd315c06c1626cbf88ae255362b9a890
Neil Roebert <neil.roebert@valr.com>
2022-12-15 16:08:35 +0200

    removed comments

--- 8-dec/main.go
+++ 8-dec/main.go
@@ -59,96 +59,73 @@ func main() {
 		fmt.Printf("%v", l)
 	}
 	fmt.Println()
-	// for _, l := range treeV {
-	// 	fmt.Printf("%v", l)
-	// }
+	for _, l := range treeV {
+		fmt.Printf("%v", l)
+	}
 
 	// this is the unefficient way of doing it :(
-	// for j := 1; j < i-1; j++ {
-	// INNER:
-	// 	for k := 1; k < g-1; k++ {
-	// 		testVal := treeH[j][k]
-	// 		fmt.Println("Test val: ", testVal)
-	// 		visH := true
-	// 		visV := true
+	for j := 1; j < i-1; j++ {
+	INNER:
+		for k := 1; k < g-1; k++ {
+			testVal := treeH[j][k]
+			fmt.Println("Test val: ", testVal)
+			visH := true
+			visV := true
 
-	// 		//vert
-	// 		for t := 0; t < i; t++ {
-	// 			// fmt.Println("Testing: [", t, "][", k, "]")
-	// 			if t == j {
-	// 				//same as test tree
-	// 				if visV {
-	// 					fmt.Println("Vis from Top")
-	// 					part_one_val++
-	// 					continue INNER
-	// 				}
-	// 				fmt.Println("Not vis from Top")
-	// 				visV = true
-	// 				continue
-	// 			}
-	// 			if treeH[t][k] >= testVal {
-	// 				visV = false
-	// 			}
+			//vert
+			for t := 0; t < i; t++ {
+				// fmt.Println("Testing: [", t, "][", k, "]")
+				if t == j {
+					//same as test tree
+					if visV {
+						fmt.Println("Vis from Top")
+						part_one_val++
+						continue INNER
+					}
+					fmt.Println("Not vis from Top")
+					visV = true
+					continue
+				}
+				if treeH[t][k] >= testVal {
+					visV = false
+				}
 
-	// 		}
-	// 		if visV {
-	// 			fmt.Println("Vis from Bot")
-	// 			part_one_val++
-	// 			continue INNER
-	// 		}
-	// 		fmt.Println("Not vis from Bot")
-	// 		// fmt.Println()
-	// 		//hor
-	// 		for y := 0; y < g; y++ {
-	// 			// fmt.Println("Testing: [", j, "][", y, "]")
-	// 			if y == k {
-	// 				//same as test tree
-	// 				if visH {
-	// 					fmt.Println("Vis from Left")
-	// 					part_one_val++
-	// 					continue INNER
-	// 				}
-	// 				fmt.Println("Not vis from Left")
-	// 				visH = true
-	// 				continue
-	// 			}
-	// 			if treeH[j][y] >= testVal {
-	// 				visH = false
-	// 			}
+			}
+			if visV {
+				fmt.Println("Vis from Bot")
+				part_one_val++
+				continue INNER
+			}
+			fmt.Println("Not vis from Bot")
+			// fmt.Println()
+			//hor
+			for y := 0; y < g; y++ {
+				// fmt.Println("Testing: [", j, "][", y, "]")
+				if y == k {
+					//same as test tree
+					if visH {
+						fmt.Println("Vis from Left")
+						part_one_val++
+						continue INNER
+					}
+					fmt.Println("Not vis from Left")
+					visH = true
+					continue
+				}
+				if treeH[j][y] >= testVal {
+					visH = false
+				}
 
-	// 		}
-	// 		if visH {
-	// 			fmt.Println("Vis from Right")
-	// 			part_one_val++
-	// 			continue INNER
-	// 		}
-	// 		fmt.Println("Not vis from Right")
-	// 		fmt.Println("-------------")
-
-	// 		// part 2
-	// 		//to top
-	// 		for t := j - 1; t >= 0; t-- {
-	// 			fmt.Println("Testing: [", t, "][", k, "]")
-	// 		}
-	// 		//to bottom
-	// 		for t := j + 1; t < i; t++ {
-	// 			fmt.Println("Testing: [", t, "][", k, "]")
-	// 		}
-
-	// 		//to left
-	// 		for y := k - 1; y >= 0; y-- {
-	// 			fmt.Println("Testing: [", j, "][", y, "]")
-
-	// 		}
-
-	// 		//to right
-	// 		for y := 0; y < g; y++ {
-	// 			fmt.Println("Testing: [", j, "][", y, "]")
-
-	// 		}
-
-	// 	}
-	// }
+			}
+			if visH {
+				fmt.Println("Vis from Right")
+				part_one_val++
+				continue INNER
+			}
+			fmt.Println("Not vis from Right")
+			fmt.Println("-------------")
+		}
+	}
 	fmt.Println("============")
 	// part 2
 	for j := 1; j < i-1; j++ {
